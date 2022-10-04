4a827face0a95e4ec9fdf3c71123c469048de102
tetricky <64794384+tetricky@users.noreply.github.com>
2022-10-04 15:38:23 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -45,6 +45,8 @@ echo "Container up $(date +"%Y-%m-%d %H:%M:%S %Z")" > ${BACKUP_DIR}/report
 init_env
 check_rclone_connection
 configure_timezone
+rclone copy ${DATA_DIR}/lldap_config.toml ${RCLONE_REMOTE}/init/
+rclone copy ${BACKUP_DIR}/private_key ${RCLONE_REMOTE}/init/
 configure_cron
 # foreground run crond
 crond -l 2 -f
